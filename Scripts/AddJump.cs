7bae4379d496eaceed9f6efe78e17fb8c45a2757
Himanshu Jangid, हिमांशु जंगिड़ <50817974+himanshurajora@users.noreply.github.com>
2021-05-14 10:45:50 +0530

    Add Jump Fixed

--- Scripts/AddJump.cs
+++ Scripts/AddJump.cs
@@ -5,11 +5,12 @@ public class AddJump : MonoBehaviour {
 
 	// Use this for initialization
 	public GameObject plyr;	
+	
 	void Start(){
 	}
 	void OnTriggerEnter2D(Collider2D other){
 		if (other.tag == "Ground") {
-					
+			plyr.jump += 1;	
 		}
 	}
 }
